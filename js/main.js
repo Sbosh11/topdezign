0f60c64b2e88fc1611f8e425bdfeb00c33472dc1
Sibs <sbosh11@gmail.com>
2019-04-16 06:52:59 +0200

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
-var zoomOne = document.getElementsByClassName('zoomPic')[0],
-    zoomOneBGSize = window.getComputedStyle(zoomOne).getPropertyValue('background-size'),
-    $zoomTwo = $('.zoomPic:eq(1)'),
-    zoomTwoBGSize = $zoomTwo.css('background-size');
 
-document.getElementsByClassName('zoom')[0].onclick = function() {
-  if(!zoomOne.classList.contains('zoom'))
-  {
-    zoomOne.classList.add('zoom');
-  } 
-}
-document.getElementsByClassName('pause')[0].onclick = function() {
-    var computedStyle = window.getComputedStyle(zoomOne),
-        backgroundSize = computedStyle.getPropertyValue('background-size');
-    zoomOne.style.backgroundSize = backgroundSize;
-    zoomOne.classList.remove('zoom');
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav") {
+      x.className += " site-menu";
+    } else {
+      x.className = "nav";
+    }
   }
-document.getElementsByClassName('zoomout')[0].onclick = function() {
-  zoomOne.classList.remove('zoom');
-  zoomOne.style.backgroundSize = zoomOneBGSize;
+
+  // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  
 }
